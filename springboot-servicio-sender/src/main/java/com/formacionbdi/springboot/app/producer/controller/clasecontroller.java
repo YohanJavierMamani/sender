3432369cacf5a3047b48823ee63bd9dde5607ee6
YohanJavierMamani <ymamani@exact.com.pe>
2020-03-05 15:36:07 -0500

    commit -m "1.5 commmit "

--- springboot-servicio-sender/src/main/java/com/formacionbdi/springboot/app/producer/controller/clasecontroller.java
+++ springboot-servicio-sender/src/main/java/com/formacionbdi/springboot/app/producer/controller/clasecontroller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.producer.service.RabbitMQSender;
 
+
 @RestController
 @RequestMapping(value = "/rabbitmq/")
 public class clasecontroller {
